95634d018fc3f4284f00cf5a4fffa86fb4925c3a
Anthony Nahas <anthony.na@hotmail.de>
2023-09-10 22:02:14 +0200

    ref(p): enhancement

---
+++ slides/s40.js
@@ -0,0 +1,14 @@
+// S40 Arrow function expression
+
+// function keyword
+const add = function add(one, two) {
+  return one + two;
+};
+
+// Fat-arrow function
+// const add = (one, two) => { return one + two }; // brackets are useless
+
+// Implicit return
+// const add = (one, two) => one + two;
+
+console.log(add(1, 2)); // 3
