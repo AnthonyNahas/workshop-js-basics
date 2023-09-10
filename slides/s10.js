659fb93a82848272200d0f3b42a80b27c51556d2
Anthony Nahas <anthony.na@hotmail.de>
2023-09-10 21:12:31 +0200

    ref(p): enhancement

---
+++ slides/s10.js
@@ -0,0 +1,20 @@
+// S10 Object property access and assignment
+
+const car = {
+  brand: 'Ford'
+};
+
+car.model = 'Mustang';
+car['full year'] = 1969;
+
+// {
+//   brand: 'Ford',
+//   model: ‘Mustang',
+//   'full year': 1969
+// }
+
+const log = (param) => {
+  console.log(param);
+};
+
+// log(a4);
---
+++ slides/s11.js
@@ -0,0 +1,10 @@
+// S11 Computed Property Names
+
+const param = 'size';
+const config = {
+  [param]: 12,
+  ['mobile' + param.charAt(0).toUpperCase() + param.slice(1)]: 4
+};
+
+console.log(config); // {size: 12, mobileSize: 4}
+
