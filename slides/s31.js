5195f1f4964fb917df296afe50f7191785a7780d
Anthony Nahas <anthony.na@hotmail.de>
2023-09-10 21:36:39 +0200

    ref(p): enhancement

---
+++ slides/s31.js
@@ -0,0 +1,11 @@
+// S31 Destructuring - Arrays
+// Get multiple local variables from an object with destructuring.
+
+const coords = [51, 6];
+
+
+const [lat, lng] = coords;
+// const lat = coords[0];
+// const lng = coords[1];
+
+console.log(lat, lng) // => 51, 6
