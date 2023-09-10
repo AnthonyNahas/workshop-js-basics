6a125bfe51b244d2830c98120cde2bd3f7a2105a
Anthony Nahas <anthony.na@hotmail.de>
2023-09-11 00:15:15 +0200

    ref(p): enhancement

--- slides/s69.js
+++ slides/s69.js
@@ -1,1 +1,15 @@
 // S69 Object (in)equality
+
+const car = {
+  brand: 'Ford'
+};
+
+const car2 = {
+  brand: 'Ford'
+};
+
+const r1 = car == car2;
+const r2 = car === car2;
+
+console.log('r1 --> ', r1);
+console.log('r2 --> ', r2);
