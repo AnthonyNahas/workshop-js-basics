1f45f73c8b5d94ef257484959b933eb1f9bb0262
Anthony Nahas <anthony.na@hotmail.de>
2023-09-11 00:22:11 +0200

    ref(p): enhancement

---
+++ slides/s77.js
@@ -0,0 +1,29 @@
+// S71 Array (in)equality
+
+
+const zero = 0;
+const anyNumber = 3;
+const anyNumberNegative = -3;
+
+console.log('zero --> ', zero);
+console.log('anyNumber --> ', anyNumber);
+console.log('anyNumberNegative --> ', anyNumberNegative);
+
+const emptyString = '';
+const anyString = 'hi';
+
+console.log('emptyString --> ', emptyString);
+console.log('anyString --> ', anyString);
+
+const braces = {};
+const list = [];
+
+console.log('braces --> ', braces);
+console.log('list --> ', list);
+
+const x = null;
+const y = undefined;
+
+console.log('x --> ', x);
+console.log('y --> ', y);
+
