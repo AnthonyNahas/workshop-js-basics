0240be27c55c0f8974370c52ba1e0b02b0f67637
Anthony Nahas <anthony.na@hotmail.de>
2023-09-10 22:33:32 +0200

    ref(p): enhancement

---
+++ slides/s56.js
@@ -0,0 +1,23 @@
+// S56 Template Literals
+
+const firstName = 'Max';
+const lastName = 'Mustermann';
+
+// create a string --> Max Mustermann
+
+const option1 = firstName + ' ' + lastName;
+console.log(option1);
+
+const option2 = `${firstName} ${lastName}`;
+console.log(option2);
+
+// const active = true;
+// const name = `Is active: ${active ? 'Yes' : 'No'}!`;
+//
+// console.log(name); // Is active: Yes!
+
+// function isActive() {
+//   return 'No';
+// }
+// const name = `Is active: ${isActive()}!`; // Is active: No!
+// console.log(name); // Is active: No!
