0180a8271287eac4f93de6145b5183ec87f324dd
Anthony Nahas <anthony.na@hotmail.de>
2023-09-10 21:15:05 +0200

    ref(p): enhancement

---
+++ slides/s15.js
@@ -0,0 +1,12 @@
+// S15 Variables - Declaration
+// Declared with the keyword var, let or const
+
+var value = 23;
+
+let anotherValue = 42;
+
+const PI = 3.1416;
+
+console.log(value); // 23
+console.log(anotherValue); // 42
+console.log(PI); // 3.1416
