8239faebf7a7fd2b64c3a6a9cc670811a8c02e23
Anthony Nahas <anthony.na@hotmail.de>
2023-09-11 00:13:35 +0200

    ref(p): enhancement

---
+++ slides/S67.js
@@ -0,0 +1,21 @@
+// S67 Check for value (in)equality
+
+const a = '1';
+const b = '1';
+const c = '2';
+
+const r1 = a === b;
+const r2 = a !== c;
+
+console.log('r1 --> ', r1);
+console.log('r2 --> ', r2);
+
+const num1 = '2';
+const num2 = '2';
+const num3 = 2;
+
+const r3 = num1 === num2;
+const r4 = num1 !== num3;
+
+console.log('r3 --> ', r3);
+console.log('r4 --> ', r4);
---
+++ slides/s69.js
@@ -0,0 +1,1 @@
+// S69 Object (in)equality
