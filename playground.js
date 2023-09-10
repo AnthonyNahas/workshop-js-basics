92969b9e8519802cd1cd23d4e1085985a78f36d3
Anthony Nahas <anthony.na@hotmail.de>
2023-09-10 22:05:43 +0200

    ref(p): enhancement

---
+++ playground.js
@@ -0,0 +1,7 @@
+const even = n => {
+  const rest = n % 2;
+
+  return rest === 0;
+};
+
+console.log(even(2));
---
+++ slides/s43.js
@@ -0,0 +1,7 @@
+// S43 Arrow function expression
+
+const person = () => ({
+  firstName: 'John',
+  lastName: 'Doe'
+});
+console.log(person()); // { firstName: 'John', lastName: 'Doe' }
