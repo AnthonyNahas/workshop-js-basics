36ea6676844c9be544e2a753e5f2c3bf14b53e92
Anthony Nahas <anthony.na@hotmail.de>
2023-09-11 00:16:41 +0200

    ref(p): enhancement

---
+++ slides/s70.js
@@ -0,0 +1,14 @@
+// S70 Object (in)equality
+
+
+const car = {
+  brand: 'Ford'
+};
+
+const car2 = car;
+
+const r1 = car == car2;
+const r2 = car === car2;
+
+console.log('r1 --> ', r1);
+console.log('r2 --> ', r2);
