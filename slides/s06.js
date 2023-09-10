3b7cf0f1fa26f61810f6ac51bb516fc379c6c8a2
Anthony Nahas <anthony.na@hotmail.de>
2023-09-10 21:06:54 +0200

    ref(p): enhancement

---
+++ slides/s06.js
@@ -0,0 +1,22 @@
+// S06 Primitive Data Types
+
+
+// Number
+const a1 = 8;
+const a2 = 4.3;
+const a3 = 0x16;
+// BigInt
+const a4 = 2n ** 53n; // BigInt is a built-in object in JavaScript that provides a way to represent whole numbers larger than 2^53 -1 = MAX_SAFE_INTEGER
+// String
+const b = 'Sophie';
+// Boolean
+const c = false;
+
+const d = null;
+const e = undefined;
+
+const log = (param) => {
+  console.log(param);
+};
+
+// log(a4);
