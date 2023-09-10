31e7ba7718308f20d6342fb4b0423363ffb1f462
Anthony Nahas <anthony.na@hotmail.de>
2023-09-11 00:11:00 +0200

    ref(p): enhancement

---
+++ slides/S66.js
@@ -0,0 +1,21 @@
+// S66 Check for value (in)equality
+
+const a = 'test';
+const b = 'test';
+const c = 'test2';
+
+const r1 = a == b;
+const r2 = a != c;
+
+console.log('r1 --> ', r1);
+console.log('r2 --> ', r2);
+
+const num1 = 1;
+const num2 = 1;
+const num3 = 2;
+
+const r3 = num1 == num2;
+const r4 = num1 != num3;
+
+console.log('r3 --> ', r3);
+console.log('r4 --> ', r4);
